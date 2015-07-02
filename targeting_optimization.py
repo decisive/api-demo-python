b7f478ff543ed7dac8893e5c7c2e4a57c6590490
Ken Hu <whosbacon@gmail.com>
2015-07-02 18:56:28 +1000

    initial example of targeting optimization

---
+++ targeting_optimization.py
@@ -0,0 +1,87 @@
+import sys # NOTE: for exiting
+
+import requests
+import datetime
+import pprint
+import ujson as json # NOTE: faster json
+
+API_KEY = '' # NOTE: input api key here!!!
+if not API_KEY:
+    sys.exit('Please insert your Decisive API key')
+
+
+print
+print 'Creating session to always add API key...'
+session = requests.Session()
+session.auth = (API_KEY,'')
+
+API_HOST = 'https://ads.decisive.is'
+def to_uri(*paths, **get_args):
+    path = '/'.join(p.strip('/') if isinstance(p,(str,unicode)) else unicode(p) for p in paths)
+    args = '&'.join('{}={}'.format(*i) for i in get_args.items())
+    return '{}/{}?{}'.format(API_HOST, path, args)
+
+def get(*paths, **get_args):
+    uri = to_uri(*paths, **get_args)
+    response = session.get(uri)
+    response.raise_for_status()
+    return response.json()
+
+def get_report(ad, type_, attribute, start_datehour, end_datehour, **kwargs):
+    return get('ads', ad['ad_id'], 'reports',
+               type_, attribute,
+               start_datehour.date().isoformat(),
+               start_datehour.hour,
+               end_datehour.date().isoformat(),
+               end_datehour.hour,
+               **kwargs)
+
+def put(updated_ad):
+    uri = to_uri('ads',updated_ad['ad_id'])
+    response = session.put(uri, data=json.dumps(updated_ad))
+    response.raise_for_status()
+    return True
+
+
+print
+print 'Selecting ad...'
+ads = get('ads', offset=1, limit=5, approved='true')
+print [a['ad_id'] for a in ads]
+ad = ads[0]
+print 'selected', ad['ad_id']
+print ad['targeting']
+
+
+print
+print 'Selecting untargeted attributes...'
+IGNORE_ATTRIBUTES = set(['city','creative_id','region','day_part','device_groups','os_version']) # NOTE: ignore attributes that are too specific
+targeted_attributes = set(ad['targeting'].keys())
+print targeted_attributes
+available_attributes = set(get('audience','attributes')) - IGNORE_ATTRIBUTES
+print available_attributes
+untargeted_attributes = set(available_attributes) - set(targeted_attributes)
+print 'selected', untargeted_attributes
+
+
+print
+print 'Identifying segments with high win potential...'
+end_datehour = datetime.datetime.now()
+start_datehour = end_datehour - datetime.timedelta(days=7)
+print 'Optimize on performances from', start_datehour, 'to', end_datehour
+select_segment = lambda r: 'unknown' not in r[attribute].lower() and r['bids'] > 10
+for attribute in untargeted_attributes:
+    report = get_report(ad, 'aggregate', attribute, 
+                        start_datehour, 
+                        end_datehour,
+                        select='bids',
+                        sort='wnr', # NOTE: win rate
+                        limit=5)
+    pprint.pprint(report); print
+    if report:
+        ad['targeting'][attribute] = [r[attribute] for r in report if select_segment(r)]
+print 'Optimized targeting', ad['targeting']
+
+sys.exit('TODO: remove once finished testing')
+print
+print 'Updating ad...'
+print 'Success?', put(ad)
